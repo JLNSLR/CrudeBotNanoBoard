d673fef406417656d1b3f29db7f97452b5cf8e4c
Julian Seiler <jseiler@mail.de>
2021-05-15 17:18:36 +0200

    Joint 1 init

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@
 #define CAN_ID_SENSORCONTROLLER_COMMAND 0x08 //CAN-ID to identify sensor controller command
 
 /* --- Board Definitions --- */
-#define JOINT_ID 2
+#define JOINT_ID 1
 #define TORQUE_SENSOR_ID (JOINT_ID - 1)
 
 /* --- Hardware Definitions --- */
